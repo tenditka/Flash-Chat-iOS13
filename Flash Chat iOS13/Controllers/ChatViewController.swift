2eafbf2cbd67eacf3eb02b1adf25b4b3503c5c61
Oksana Fedorchuk <klassi4eske@gmail.com>
2019-11-27 13:09:23 +0200

    7. Added : - Model for Message - Array messages containing [Message] based on model - Extension for ChatViewController to adopt UITableViewDataSource, which contains two functions, one returns number of rows in the tableView and another returns a reusable cell. - Set tableView.dataSource to self

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,8 +14,15 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "oksana@gmail.com", body: "Hi!"),
+        Message(sender: "ok@gmail.com", body: "Hello!"),
+        Message(sender: "oksana@gmail.com", body: "What's up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
 
@@ -33,8 +40,20 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
           print ("Error signing out: %@", signOutError)
         }
-          
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    // How many rows in tableview? Returns number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    // Create a reusable cell and return it to the tableview
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
     }
     
-
+    
 }
