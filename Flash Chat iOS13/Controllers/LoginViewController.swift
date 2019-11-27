16fa2f9f91a53d09b4a3a3a0714ff733a7085e89
Oksana Fedorchuk <klassi4eske@gmail.com>
2019-11-27 10:17:53 +0200

    4. Adding login function with pop-up errors

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -15,6 +16,22 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        
+            if let email = emailTextfield.text, let password = passwordTextfield.text {
+                Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                  if let errorAlert = error {
+                      let alert = UIAlertController(title: "Unable to login", message: errorAlert.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+
+                      // add an action (button)
+                      alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+                      // show the alert
+                      self.present(alert, animated: true, completion: nil)
+                      
+                  } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                  }
+                }
+            }
     }
-    
 }
